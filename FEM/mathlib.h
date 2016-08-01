3430bf5aa8c6adfab27ac0311cc3cb979744b11e
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-08-01 12:59:08 +0200

    fix OGS-PETSc build

--- FEM/mathlib.h
+++ FEM/mathlib.h
@@ -28,21 +28,6 @@
 
 #define noTESTMATH
 
-/* Andere oeffentlich benutzte Module */
-//#include "test.h"
-
-/* Die Schnittstellen der Gleichungsloeser und der Speichertechnik werden
-   sozusagen durchgeschleift: */
-/* #include "matrix_routines.h" */
-/* Speichertechnik fuer Matrix des Gesamtgleichungssystems */
-#ifndef NEW_EQS  // WW. 11.2008
-#include "solver.h"
-#endif
-/* Iterative GLS-Loeser auf Basis der Speichertechnik aus 'matrix.h'
- * (herkoemmliche Verfahren) */
-
-// C++
-//#include <vector>
 
 /*##########################################################################
       Mathematische Funktionen
