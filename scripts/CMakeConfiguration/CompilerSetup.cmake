f6c62c1521e9342555fa3d22673c5d110c1ab260
Norihiro Watanabe <norihiro.watanabe@ufz.de>
2016-08-09 08:05:42 +0200

    [CMake] use CCACHE

--- scripts/CMakeConfiguration/CompilerSetup.cmake
+++ scripts/CMakeConfiguration/CompilerSetup.cmake
@@ -21,6 +21,12 @@ if(NOT COMPILER_IS_MSVC)
     set( CMAKE_INCLUDE_SYSTEM_FLAG_CXX "-isystem" CACHE STRING "" FORCE )
 endif()
 
+find_program(CCACHE_FOUND ccache)
+if(CCACHE_FOUND)
+	message(STATUS "CCACHE found")
+	set_property(GLOBAL PROPERTY RULE_LAUNCH_COMPILE ccache)
+	set_property(GLOBAL PROPERTY RULE_LAUNCH_LINK ccache)
+endif(CCACHE_FOUND)
 
 IF (WIN32)
 	## For Visual Studio compiler
